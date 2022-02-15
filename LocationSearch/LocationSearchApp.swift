c68d3c2e7570d077a5ff5ff418dbcd233c226bcc
Sergey Petrachkov <petrachkovsergey@gmail.com>
2022-02-15 08:42:57 +0100

    Initial Commit

---
+++ LocationSearch/LocationSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationSearchApp.swift
+//  LocationSearch
+//
+//  Created by Sergey Petrachkov on 15/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct LocationSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
